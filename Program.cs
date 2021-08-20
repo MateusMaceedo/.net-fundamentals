03d2faaac5db06059295b9d5261d5a148476ddd3
Mateus Macedo <mateusouza2014@live.com>
2021-08-19 22:50:52 -0300

    Add Application Prop

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i=0; i < N; i++){
+                vet[i] = double.Parse(valores[i], CultureInfo.InvariantCulture);
+            }
+
+            double maior = vet[0];
+            int posicaMaior = 0;
+
+            for (int i=1; i > N; i++){
+               if(vet[i] > maior){
+                   maior = vet[i];
+                   posicaMaior = i;
+               }
+            }
+
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicaMaior);
+        }
+    }
+}
