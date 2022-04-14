8bc000307cb74387403383bd3b4645280c81d364
MateusMacedo <mateusouza2014@live.com>
2022-04-13 23:24:31 -0300

    test: Adicionando testes a solução

--- ParseETryParse/Program.cs
+++ ParseETryParse/Program.cs
@@ -1,21 +1,17 @@
-﻿using System.Globalization;
+﻿using System;
 
-var valor1 = int.Parse("123");
-var valor2 =  bool.Parse("true");
-var valor3 = DateTime.Parse("28/01/2022");
-
-Console.WriteLine(valor1);
-Console.WriteLine(valor2);
-Console.WriteLine(valor3);
-
-
-// Parse
-var culture = new CultureInfo("pt-BR");
-var valorParse = DateTime.Parse("28/10/2022");
-
-Console.WriteLine(valorParse);
-
-// TryParse
-var deuBom = int.TryParse("123456", out var valorTryParse);
-Console.WriteLine(deuBom);
-Console.WriteLine(valorTryParse);
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
